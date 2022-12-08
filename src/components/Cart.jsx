1660ea00ef87994afc3ede6da0d6ce7dd554be9c
martina deipenau <martinadeipenau02@gmail.com>
2022-12-08 19:48:24 -0300

    add:

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -6,46 +6,78 @@ import { db } from '../utils/firebaseConfig';
 import FormatNumber from "../utils/FormatNumber"
 import { doc, setDoc } from "firebase/firestore";
 
-
 const Cart = () => {
     const test = useContext(CartContext)
 
     const createOrder = () => {
-        const itemsForDB = test.cartList.map(item => ({
-            id: item.IdItem,
-            title: item.titleItem,
-            price: item.priceItem
-        }))
-        test.cartList.forEach(async (item) => {
-            const itemRef = doc(db, "products", item.idItem)
-            await updateDoc(itemRef, {
-                stock: increment(-item.qtyItem)
-            })
-        })
-
         let order = {
             buyer: {
-                name: "Emma Sophi",
-                email: "emma@sophi.com",
-                phone: "46521632"
+                name: 'Emma Sophi',
+                email: 'emma@sophi.com',
+                phone: '515652226'
             },
-            total: test.calcTotal(),
-            items: itemsForDB,
-            date: serverTimestamp()
+            date: serverTimestamp(),
+            items: test.cartList.map(item => ({
+                id: item.idItem,
+                title: item.titleItem,
+                price: item.priceItem,
+                qty: item.qtyItem
+            })),
+            total: test.calcTotal()
         }
-        console.log(order)
 
-        const createOrderInFireS = async () => {
-            const newOrderRef = doc(collection(db, "orders"))
-            await setDoc(newOrderRef, order)
+        const createOrderInDB = async() => {
+            const newOrderRef = doc(collection(db, 'orders'))
+            await setDoc(newOrderRef, order);
             return newOrderRef
         }
-        createOrderInFireS()
-            .then(result => alert('Tu orden de compra se creo'))
-            .catch(error => console.log(error))
 
-        test.removeList()
+        createOrderInDB()
+        .then(response => console.log('Orden de compra en la BD', response.id))
+        .catch(error => console.log(error))
     }
+
+
+
+    // const Cart = () => {
+    //     const test = useContext(CartContext)
+
+    //     const createOrder = () => {
+    //         const itemsForDB = test.cartList.map(item => ({
+    //             id: item.IdItem,
+    //             title: item.titleItem,
+    //             price: item.priceItem
+    //         }))
+    //         test.cartList.forEach(async (item) => {
+    //             const itemRef = doc(db, "products", item.idItem)
+    //             await updateDoc(itemRef, {
+    //                 stock: increment(-item.qtyItem)
+    //             })
+    //         })
+
+    //         let order = {
+    //             buyer: {
+    //                 name: "Emma Sophi",
+    //                 email: "emma@sophi.com",
+    //                 phone: "46521632"
+    //             },
+    //             total: test.calcTotal(),
+    //             items: itemsForDB,
+    //             date: serverTimestamp()
+    //         }
+    //         console.log(order)
+
+    //         const createOrderInFireS = async () => {
+    //             const newOrderRef = doc(collection(db, "orders"))
+    //             await setDoc(newOrderRef, order)
+    //             return newOrderRef
+    //         }
+    //         createOrderInFireS()
+    //             .then(response => alert('Tu orden de compra se creo'))
+    //             .catch(error => console.log(error))
+
+    //         test.removeList()
+    //     }
 
 
     return (
@@ -66,18 +98,17 @@ const Cart = () => {
                             test.cartList.length > 0 &&
                             test.cartList.map(item =>
                                 <p id={item.idItem}>
-                                    <div>
+                                    <div className="Compra">
                                         <img src={item.imageItem} />
                                         <div>
-                                            <p>{item.titleItem}</p> 
+                                            <p>{item.titleItem}</p>
                                         </div>
                                         <button onClick={() => test.deleteItem(item.idItem)}>Borrar</button>
                                     </div>
                                     <div>
                                         <div>
                                             <p>{item.qtyItem} item(s)</p>
-                                            /
-                                            <p>${item.priceItem} each </p>
+                                            <p>${item.priceItem} cada uno </p>
                                         </div>
                                         <p>$ {test.calcularTotalPerItem(item.idItem)}</p>
                                     </div>
@@ -87,7 +118,7 @@ const Cart = () => {
                     </div>
                     {
                         test.cartList.length > 0 &&
-                        <div>
+                        <div className="OrdendeCompra">
                             <title>Orden de compra</title>
                             <div>
                                 <p>Subtotal</p>
@@ -95,9 +126,9 @@ const Cart = () => {
                             </div>
                             <div className="TotalCompra" >
                                 <p>Total</p>
-                                <p> <FormatNumber number={test.calcTotal()} /></p>
+                                <p><FormatNumber number={test.calcTotal()} /></p>
                             </div>
-                            <button>COMPRAR</button>
+                            <button className="Buttoncomprar">COMPRAR</button>
                         </div>
                     }
                 </div>
@@ -105,4 +136,5 @@ const Cart = () => {
         </>
     )
 }
+
 export default Cart;
